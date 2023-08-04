fa54513f75258870a4fcbc5968eb65de8847dd92
C Black <blackc301x@gmail.com>
2023-08-04 23:34:54 +0800

    1、完成了管理员权限获取的功能

--- RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl.cpp
@@ -102,6 +102,7 @@ void ShowError() {
         NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
         (LPWSTR)&lpMessageBuf, 0, NULL);
     OutputDebugString(lpMessageBuf);
+    MessageBox(NULL, lpMessageBuf, _T("发生错误"), 0);
     LocalFree(lpMessageBuf);
 }
 
@@ -125,14 +126,39 @@ bool IsAdmin() {
     return false;
 }
 
+void RunAsAdmin() {
+//TODO：获取管理员权限、使用该权限创建进程
+    HANDLE hToken = NULL;
+    //LOGON32_LOGON_BATCH
+    //LOGON32_LOGON_INTERACTIVE
+    BOOL ret = LogonUser(L"Administrator", NULL, NULL, LOGON32_LOGON_BATCH, LOGON32_PROVIDER_DEFAULT, &hToken);
+    if (!ret) {
+        ShowError();
+        MessageBox(NULL, _T("登录错误"), _T("程序错误"), 0);
+        ::exit(0);
+    }
+    OutputDebugString(L"Logon administrator success!\r\n");
+    STARTUPINFO si = { 0 };
+    PROCESS_INFORMATION pi = { 0 };
+    TCHAR sPath[MAX_PATH] = _T("");
+    GetCurrentDirectory(MAX_PATH, sPath);
+    CString strCmd = sPath;
+    strCmd += _T("\\RemoteCtrl.exe");
+    //ret = CreateProcessWithTokenW(hToken, LOGON_WITH_PROFILE, NULL, (LPWSTR)(LPCWSTR)strCmd, CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi);
+    ret = CreateProcessWithLogonW(_T("Administrator"), NULL, NULL, LOGON_WITH_PROFILE, NULL, (LPWSTR)(LPCWSTR)strCmd, CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi);
+    CloseHandle(hToken);
+    if (!ret) {
+        ShowError();
+        MessageBox(NULL, strCmd, _T("创建进程失败"), 0);
+        ::exit(0);
+    }
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+}
+
 int main()
 {
-    if (IsAdmin()) {
-        OutputDebugString(L"Current is run as administrator!\r\n");
-    }
-    else {
-        OutputDebugString(L"Current is run as normal user!\r\n");
-    }
     int nRetCode = 0;
 
     HMODULE hModule = ::GetModuleHandle(nullptr);
@@ -148,6 +174,16 @@ int main()
         }
         else
         {
+            if (IsAdmin()) {
+                OutputDebugString(L"Current is run as administrator!\r\n"); 
+                MessageBox(NULL, _T("管理员"), _T("用户状态"), 0);
+            }
+            else {
+                OutputDebugString(L"Current is run as normal user!\r\n");
+                RunAsAdmin();
+                MessageBox(NULL, _T("普通用户"), _T("用户状态"), 0);
+                return nRetCode;
+            }
             CCommand cmd;
             ChooseAutoInvoke();
             int ret = CServerSocket::getInstance()->Run(&CCommand::RunCommand, &cmd);
